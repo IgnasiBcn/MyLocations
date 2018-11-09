8454d083e3fafb73f30e0cba0301a21a3302275e
iMac <user@imac-de-user.local>
2018-11-09 08:08:21 +0100

    Initial Commit

---
+++ MyLocations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyLocations
+//
+//  Created by iMac on 9/11/18.
+//  Copyright © 2018 Ignasi Pérez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
